4751926e56ffe9794eca3602cb50d724ed93828d
yishan <yishan@protonmail.com>
2020-05-10 03:08:15 +0200

    Update Stallman.py

--- Stallman.py
+++ Stallman.py
@@ -1,108 +1,39 @@
-import asyncio
-import json
-import random
-import sys
-import time
-from urllib.parse import quote as urlquote
-from urllib.request import urlopen
+# stallman.py
+# Bot discord de yishan
 
+import os
 import discord
-import requests
-import urbandictionary as ud
-from bs4 import BeautifulSoup
 
-UD_DEFID_URL = 'https://api.urbandictionary.com/v0/define?defid='
-UD_DEFINE_URL = 'https://api.urbandictionary.com/v0/define?term='
-UD_RANDOM_URL = 'https://api.urbandictionary.com/v0/random'
-class UrbanDefinition(object):
-    def __init__(self, word, definition, example, upvotes, downvotes):
-        self.word = word
-        self.definition = definition
-        self.example = example
-        self.upvotes = upvotes
-        self.downvotes = downvotes
+from dotenv import load_dotenv
+from discord.ext import commands
 
-    def __str__(self):
-        return '%s: %s%s (%d, %d)' % (
-                self.word,
-                self.definition[:50],
-                '...' if len(self.definition) > 50 else '',
-                self.upvotes,
-                self.downvotes
-            )
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
+bot = commands.Bot(command_prefix='!')
 client = discord.Client()
 
-# Mise en route
-async def on_ready():
-    print("Logged in as:", client.user.name)
-    print("ID:", client.user.id)
-
-# Ping 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+async def on_ready():
+    game = discord.Game('Attends que Yιѕнαи le code')
+    await client.change_presence(status=discord.Status.online,activity=game)
 
-    elif message.content == "!ping":
-        timePing = time.monotonic()
-        pinger = await client.send_message(message.channel, ":ping_pong: **Pong !**")
-        ping = '%.2f' % ( 1000 * (time.monotonic() - timePing))
-        await client.edit_message(pinger, ":ping_pong: **Pong !**\n `Latence : " + ping + " ms`" )
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
-# Hello
+    print(
+        f'{client.user} est connecté sur le discord suivant : \n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
 
-    elif message.content == "!hello":
-        await client.send_message(message.channel, "Hello!")
+@bot.command(name='ping', help='réponds au ping')
+async def ping(ctx):
+    test_ping = 'pong'
 
-# UrbanDictionary
-      
-    elif message.content == "!urban":
-        
-        
-        def _get_urban_json(url):
-            f = urlopen(url)
-            data = json.loads(f.read().decode('utf-8'))
-            f.close()
-            return data
-        
-        def _parse_urban_json(json, check_result=True):
-            result = []
-            if json is None or any(e in json for e in ('error', 'errors')):
-                raise AttributeError('UD: Invalid input for Urban Dictionary API')
-            if check_result and ('list' not in json or len(json['list']) == 0):
-                return result
-            for definition in json['list']:
-                d = UrbanDefinition(
-                        definition['word'], 
-                        definition['definition'],
-                        definition['example'],
-                        int(definition['thumbs_up']),
-                        int(definition['thumbs_down'])
-                    )
-                result.append(d)
-            return result
+    response = (test_ping)
+    await ctx.send(response)
 
-        '''def define(term):
-            """Search for term/phrase and return list of UrbanDefinition objects.
-            Keyword arguments:
-            term -- term or phrase to search for (str)
-            """
-            json = _get_urban_json(UD_DEFINE_URL + urlquote(term))
-            return _parse_urban_json(json)
-
-        def defineID(defid):
-            """Search for UD's definition ID and return list of UrbanDefinition objects.
-            Keyword arguments:
-            defid -- definition ID to search for (int or str)
-            """
-            json = _get_urban_json(UD_DEFID_URL + urlquote(str(defid)))
-            return _parse_urban_json(json)
-
-        def random():
-            """Return random definitions as a list of UrbanDefinition objects."""
-            json = _get_urban_json(UD_RANDOM_URL)
-        return _parse_urban_json(json, check_result=False)'''
-        
-# ID
-client.run("NTQzNDQ5Nzk3NDc5MTA0NTEy.XKeXJA.ZkhDIMixUjuJMG2VvWskRSOeK-Q")
+client.run(TOKEN)
+bot.run(TOKEN)
